a5067a56b1f8d0bb8365326dddaa1febdb2b7844
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-05-11 12:59:15 +0900

    before_actionでツイートを探す処理をまとめる

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-
+  before_action :set_tweet, only: [:edit, :show]
   def index
     @tweets = Tweet.all
   end
@@ -18,7 +18,6 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
@@ -27,12 +26,15 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
   
 end
